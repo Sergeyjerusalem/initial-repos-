6bf688b7389f41990519b02a804afe7c33da2f28
Sergey <lolka026@gmail.com>
2021-09-20 17:22:36 +0300

    test

--- main.js
+++ main.js
@@ -2,46 +2,50 @@ const fs = require("fs-extra");
 
 
 fs.mkdir('new_folder', err => {
-   if(err) throw err; // не удалось создать папку
-   console.log('folder1 is created');
-});
+  if (err) throw err; // не удалось создать папку
+  console.log('folder1 is created');
 
-
-fs.writeFile("/Users/pro/lol/new_folder/filename.txt", "Текст", function(err){
+  fs.writeFile("/Users/pro/lol/new_folder/filename.txt", "Текст", function (err) {
     if (err) {
-    console.log(err);
+      console.log(err);
     } else {
-    console.log("file is created");
+      console.log("file is created");
     }
-    });
+  });
+  fs.mkdir('new_folder2', err => {
+    if (err) throw err; // не удалось создать папку
+    console.log('folder2 is created');
+  });
 
-fs.mkdir('new_folder2', err => {
-        if(err) throw err; // не удалось создать папку
-        console.log('folder2 is created');
-        });
 
-
-        fs.copyFile("/Users/pro/lol/new_folder/filename.txt", "/Users/pro/lol/new_folder2/filename.txt", err => {
-if (err) return console.error(err)
-console.log('copied')
-});
-
-fs.remove("/Users/pro/lol/new_folder/filename.txt", err => {
+  fs.copyFile("/Users/pro/lol/new_folder/filename.txt", "/Users/pro/lol/new_folder2/filename.txt", err => {
     if (err) return console.error(err)
-    console.log('file removed!') 
+    console.log('copied')
+  });
+
+  fs.remove("/Users/pro/lol/new_folder/filename.txt", err => {
+    if (err) return console.error(err)
+    console.log('file removed!')
   });
 
   fs.remove("/Users/pro/lol/new_folder2/filename.txt", err => {
     if (err) return console.error(err)
-    console.log('file2 removed!') 
+    console.log('file2 removed!')
   });
-  
+
   fs.remove("/Users/pro/lol/new_folder/", err => {
     if (err) return console.error(err)
-    console.log('folder1removed!') 
+    console.log('folder1removed!')
   });
 
   fs.remove("/Users/pro/lol/new_folder2", err => {
     if (err) return console.error(err)
-    console.log('folder2 removed!') 
+    console.log('folder2 removed!')
   });
+
+
+});
+
+
+
+
